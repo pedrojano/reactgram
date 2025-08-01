39853da671c4a2006855ae6848dec11b1d1db8af
Pedro Henrique Janó <pedrino022@gmail.com>
2025-08-01 17:19:47 -0300

    Foi adiconado a rota delete para deletar as fotos

--- controllers/PhotoController.js
+++ controllers/PhotoController.js
@@ -22,12 +22,44 @@ const insertPhoto = async (req, res) => {
         res.status(422).json({
             errors: ["Houve um problema, por favor tente novamente mais tarde!"]
         });
+        return
     }
 
     res.status(201).json(newPhoto);
 };
 
+// Remove a photo from DB
+const deletePhoto = async(req,res) => {
+    const {id} = req.params;
+    const reqUser = req.user;
+
+    try {
+        const photo = Photo.findById(mongoose.Types.ObjectId(id));
+
+        // Check if photo exists
+        if(!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada!"]});
+            return;
+        }
+
+        // Check if photo belong to user
+        if(!photo.userId.equals(reqUser._id)) {
+            res.status(422).json({
+                errors: ["Ocorreu um erro, tente novamente mais tarde!"],
+            });
+        }
+
+        await Photo.findByIdAndDelete(photo._id);
+        res.status(200).json({id: photo._id, message: "Foto exclúida com sucesso!"})
+    } catch (error) {
+         res.status(404).json({ errors: ["Foto não encontrada!"]});
+            return;
+    }
+
+};
+
 module.exports = {
     insertPhoto,
+    deletePhoto,
 }
 
